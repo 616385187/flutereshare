0ef4c1adb372be2c755eecc7ad74da104b70d3f1
zzj <184195794@qq.com>
2018-06-21 00:50:18 +0800

    ios端获取Eshare设备

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,65 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <EShare/EShare.h>
+
+@interface AppDelegate()
+@property (nonatomic,strong)FlutterViewController* controller;
+@property (nonatomic,copy)void (^deviceBlock)(NSArray *devicesArray);
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    self.controller = (FlutterViewController*)self.window.rootViewController;
+    FlutterMethodChannel* batteryChannel = [FlutterMethodChannel
+                                            methodChannelWithName:@"GET_DEVICE_CHANNEL"
+                                            binaryMessenger:self.controller];
+    
+    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        // TODO
+        
+        if ([call.method isEqualToString:@"GET_DEVICE_CHANNEL_Fx"])
+        {
+            self.deviceBlock = ^(NSArray *devicesArray) {
+                result(devicesArray);
+            };
+            [self getDevice];
+            
+
+        }
+        else if ([call.method isEqualToString:@"GET_DEVICE_CHANNEL_TRYTOCONNECT"])
+        {
+            result(nil);
+        }
+        else if ([call.method isEqualToString:@"GET_DEVICE_CHANNEL_FILE"])
+        {
+            result(nil);
+        }
+        else if ([call.method isEqualToString:@"SHOWFILE"])
+        {
+            result(nil);
+        }
+        else
+        {
+            result(FlutterMethodNotImplemented);
+        }
+    }];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
+- (void)getDevice
+{
+    [[ESDeviceCommand sharedESDeviceCommand] getDeviceList:^(NSArray *deviceAry) {
+        
+        if (self.deviceBlock)
+        {
+            self.deviceBlock(deviceAry);
+        }
+    }];
+}
+
 
 @end
